46ea1067e4e81778f54e9cee22ff07825dc8be23
Sam Passarelli <passarelli.sam@gmail.com>
2020-09-01 02:35:37 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,10 @@
 # don't forget to add: require 'pry'
-
+require 'pry'
 def snake_it_up(string)
   if string[0] == "s"
-    "s" * 10 + string
+    10 * "s" + string
   else
     string
   end
 end
+binding.pry
