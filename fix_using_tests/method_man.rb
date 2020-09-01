c97ac34b1f8edd338d82b4654e1fe52e1b3b5b8c
Sam Passarelli <passarelli.sam@gmail.com>
2020-09-01 01:55:40 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,4 +1,4 @@
-require 'pry'
+
 
 def start_game(player1, player2)
   "Hello #{player1} & #{player2}"
@@ -7,4 +7,3 @@ end
 def play_game(player1,player2)
   "#{player1} is better than #{player2}"
 end
-binding.pry
